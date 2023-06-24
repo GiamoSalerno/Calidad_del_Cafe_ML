ea24669b3bbfe6620c97dae31b7ba564744e68d0
Giacomo <giamosalerno@gmail.com>
2023-06-24 18:19:58 +0200

    Actualización EDA y parámetros de configuración.

--- src/model.py
+++ src/model.py
@@ -1,29 +1,34 @@
 # Importo librerías necesarias
-import pandas as pd, pickle
+import pandas as pd, pickle, yaml
 from sklearn.decomposition import PCA
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.feature_selection import SelectKBest
 from sklearn.pipeline import Pipeline
 
+# Defino el configurador.
+with open('models/model_config.yaml', 'r') as file:
+    yam = yaml.safe_load(file)
+
+# Datos de entrenamiento
+train = pd.read_csv(yam['data']['train_file'])
+X = train.drop(columns = yam['data']['target_column'])
+y = train[yam['data']['target_column']]
+
 # Modelo final a entrenar
-best_params = {'selectkbest__k': 9,
-               'pca__n_components': 9,
-               'classifier': GradientBoostingClassifier(max_depth=7, max_features=2)}
+best_params = {'selectkbest__k': yam['model']['pipeline']['steps'][0]['params']['k'],
+               'pca__n_components': yam['model']['pipeline']['steps'][1]['params']['n_components'],
+               'classifier': GradientBoostingClassifier(max_depth = yam['model']['pipeline']['steps'][2]['params']['max_depth'],
+                                                        max_features = yam['model']['pipeline']['steps'][2]['params']['max_features'])}
 
 best_model = Pipeline(steps=[("selectkbest", SelectKBest(k=best_params['selectkbest__k'])),
                              ("pca", PCA(n_components=best_params['pca__n_components'])),
                              ("classifier", best_params['classifier'])])
 
-# Datos de entrenamiento
-train = pd.read_csv('data/train/arabica_train.csv')
-X = train.drop(columns='Calidad')
-y = train.Calidad
-
 # Entrenamiento
 best_model.fit(X, y)
 
 # Guardado del modelo entrenado
-with open('models/modelo_final.pkl', 'wb') as file:
+with open(yam['output_file'], 'wb') as file:
     pickle.dump(best_model, file)
 
 # Mensaje final
